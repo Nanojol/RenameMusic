fcbfb5f7bfa50a3755feb818bf15b766598c0fed
NevO <Nefedovkirill@mail.ru>
2025-08-31 11:24:04 +0400

    # Добавляет цифры в префикс # Удаляет числа из префикса # Добавляет числа из списка в префикс файла

--- Generate.py
+++ Generate.py
@@ -1,11 +1,61 @@
-import os
+# Добавляет цифры в префикс
+# import os
+#
+# path = r'C:\Users\NevO\Desktop\rename'
+# count = 2
+# for root, dirs, files in os.walk(path):
+#     for _file in files:
+#         os.rename(root + "\\" + _file, root + "\\" + str(count) + ". " + _file)
+#         count += 2
+# print("конец")
 
-path = r'C:\Users\NevO\Desktop\Музыка'
-count = 2
+
+# Удаляет числа из префикса
+# import os
+# import re
+#
+# path = r'C:\Users\NevO\Desktop\Ренэйм'
+# # Исходный путь к файлу
+# for root, dirs, files in os.walk(path):
+#     for _file in files:
+#         old_path = root + "\\" + _file
+#
+#         # Получаем директорию и имя файла
+#         directory, filename = os.path.split(old_path)
+#
+#         # Разделяем имя файла и расширение
+#         name, ext = os.path.splitext(filename)
+#
+#         # Удаляем числовой префикс в начале имени файла
+#         new_name = re.sub(r'^\d+\.\s*', '', name)
+#
+#         # Формируем новое имя файла
+#         new_filename = new_name + ext
+#
+#         # Полный путь для нового файла
+#         new_path = os.path.join(directory, new_filename)
+#
+#         # Переименование файла
+#         os.rename(old_path, new_path)
+# print("конец")
+
+# Добавляет числа из списка в префикс файла
+
+import os
+import random
+
+path = r'C:\Users\NevO\Desktop\rename'
+# Количество элементов в папке
+count = 297
+count_list = [x for x in range(1, count+1, 1)]
+# print(count_list)
+random.shuffle(count_list)
+
+# print(number)
+
 for root, dirs, files in os.walk(path):
     for _file in files:
-        # print(_file)
-        os.rename(root + "\\" + _file, root + "\\" + str(count) + ". " + _file)
-        # print(_file)
-        count += 2
+        number = count_list.pop(0)
+        os.rename(root + "\\" + _file, root + "\\" + str(number) + ". " + _file)
 
+print("конец")
